8ea25bd83f3591fdb703497940cbcbd8ac2db830
moabdels <mkabdelsater@gmail.com>
2024-11-21 13:57:06 +0200

    ADD mlx_trgb.c to create and manipulate trgb color values

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:07:13 by moabdels          #+#    #+#             */
-/*   Updated: 2024/11/21 12:31:36 by moabdels         ###   ########.fr       */
+/*   Updated: 2024/11/21 13:26:05 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,12 @@ typedef struct s_img_data
 }	t_img_data;
 
 void	ft_pixel_put(t_img_data *data, int x, int y, int color);
+
+// $ TRGB Functions
+int	create_trgb(int t, int r, int g, int b);
+int	get_trgb_t(int trgb);
+int	get_trgb_r(int trgb);
+int	get_trgb_g(int trgb);
+int	get_trgb_b(int trgb);
 
 #endif
