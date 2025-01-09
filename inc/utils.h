2868dbabad00673311cbb4cb4df6f12d7bc4a3d5
moabdels <mkabdelsater@gmail.com>
2025-01-09 15:00:41 +0200

    ADD missing ifndef statement

--- inc/utils.h
+++ inc/utils.h
@@ -6,9 +6,13 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 15:25:51 by moabdels          #+#    #+#             */
-/*   Updated: 2025/01/09 13:18:30 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/01/09 15:00:17 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef UTILS_H
+# define UTILS_H
+
 int		gradient(int start_color, int end_color, int len, int pixels);
 void	free_2d_vector(char **vec);
+#endif
