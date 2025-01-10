7209a2b974caa77385ed9a7a52bf1511712dd4d4
moabdels <mkabdelsater@gmail.com>
2025-01-10 13:36:40 +0200

    ADD system_init to main

--- src/main.c
+++ src/main.c
@@ -6,12 +6,32 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 11:55:14 by moabdels          #+#    #+#             */
-/*   Updated: 2025/01/09 15:12:42 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/01/10 13:35:02 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
 #include "../inc/map.h"
+
+void	system_init(t_globals *global_state)
+{
+	global_state->map.renders = 0;
+	global_state->map.proportion = global_state->map.limits.axis[Z_AXIS] / \
+		global_state->map.limits.axis[X_AXIS];
+	if (global_state->map.proportion > 0.5)
+		global_state->map.z_divisor = global_state->map.proportion * 30;
+	global_state->keys.b_key_ctrl = 0;
+	global_state->keys.b_mouse_l = 0;
+	global_state->keys.b_mouse_r = 0;
+	global_state->mlx = mlx_init();
+	global_state->win = mlx_new_window(global_state->mlx, WIN_WIDTH, \
+		WIN_HEIGHT,"FDF - moabdels");
+	global_state->bitmap.img = mlx_new_image(global_state->mlx, WIN_WIDTH, \
+		WIN_HEIGHT);
+	global_state->bitmap.buffer = mlx_get_data_addr(global_state->bitmap.img, \
+		&global_state->bitmap.bit_x_pixel, &global_state->bitmap.lines, \
+		&global_state->bitmap.endian);
+}
 
 void	error_out(char *err_msg)
 {
@@ -27,7 +47,7 @@ int	main(int argv, char **argc)
 	if (argv != 2)
 		error_out("Number of arguments not 1 - Usage : `./fdf <map file>`");
 	load_map(&global_state.map, argc[1]);
-	(void)global_state;
+	system_init(&global_state);
 	(void)argc;
 	exit(EXIT_SUCCESS);
 }
