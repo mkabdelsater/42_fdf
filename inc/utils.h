eb18ac93948b65b883ff6739014fe555fe6b61f3
moabdels <mkabdelsater@gmail.com>
2025-01-20 15:24:58 +0200

    Add draw_number and draw_string

--- inc/utils.h
+++ inc/utils.h
@@ -6,7 +6,7 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 15:25:51 by moabdels          #+#    #+#             */
-/*   Updated: 2025/01/09 15:00:17 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/01/20 15:24:32 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,7 @@
 
 int		gradient(int start_color, int end_color, int len, int pixels);
 void	free_2d_vector(char **vec);
+void	draw_number(t_globals *global_state, int x_coord, int y_coord, int nbr);
+void	draw_string(t_globals *global_state, int x_coord, int y_coord, char *str);
+
 #endif
