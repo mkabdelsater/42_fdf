d05ee832c1c799388cd87e06bc6c15a875b15ab2
moabdels <mkabdelsater@gmail.com>
2025-01-20 15:41:36 +0200

    Implement draw_controls

--- src/draw_menu.c
+++ src/draw_menu.c
@@ -6,7 +6,7 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 15:02:22 by moabdels          #+#    #+#             */
-/*   Updated: 2025/01/20 15:12:23 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/01/20 15:41:26 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,28 @@ static void	draw_controls(t_globals *global_state)
 	int	line;
 
 	line = COLORSCHEME_BOX;
-	print
+	draw_string(global_state, MENU_TAB_SIZE, line, "---- CONTROLS ----");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "Scroll: Zoom In/Out");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "LMB: Rotate Model (X/Y Axis)");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "Arrow Keys: Rotate along X/Y");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "RMB: Translate Model");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "Q,E: Rotate Z Axis");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "W,S: Rotate Y Axis");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "A,D: Rotate X Axos");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "+/- Z Divisor");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "B : Blending");
+	line += LINE_SIZE;
+	draw_string(global_state, MENU_TAB_SIZE, line, "I,F,C: Isometric/Fit/Center");
+	line += LINE_SIZE;
 }
 
 void	draw_menu(t_globals *global_state)
