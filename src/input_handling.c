7c311bff74398e5c4b96a953c25d8e52346c2c7b
moabdels <mkabdelsater@gmail.com>
2025-01-21 15:17:12 +0200

    Add rotation_control to input_handling

--- src/input_handling.c
+++ src/input_handling.c
@@ -6,11 +6,25 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 12:56:38 by moabdels          #+#    #+#             */
-/*   Updated: 2025/01/21 15:03:17 by moabdels         ###   ########.fr       */
+/*   Updated: 2025/01/21 15:15:54 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
+#include "../inc/keycodes.h"
+
+// !TO_REFACTOR
+
+static void modify_angle(float *angle, float mod)
+{
+	*angle += mod;
+	if (*angle < 0)
+		*angle = 360 + *angle;
+	if (*angle >=360)
+		*angle = *angle - 360;
+}
+
+// !TODO : Add wasdqe control scheme
 
 static void	rotation_control(int key, t_globals *fdf)
 {
@@ -19,7 +33,18 @@ static void	rotation_control(int key, t_globals *fdf)
 	ang = 1;
 	if (fdf->keys.b_key_ctrl)
 		ang = 90;
-	
+	if (key == KEY_DOWN)
+		modifiy_angle(&fdf->map.ang[X_AXIS], ang);
+	if (key == KEY_UP)
+		modify_angle(&fdf->map.ang[X_AXIS], -ang);
+	if (key == KEY_LEFT)
+		modify_angle(&fdf->map.ang[Y_AXIS], ang);
+	if (key == KEY_RIGHT)
+		modify_angle(&fdf->map.ang[Y_AXIS], -ang);
+	if (key == KEY_Q)
+		modify_angle(&fdf->map.ang[Z_AXIS], ang);
+	if (key == KEY_W)
+		modify_angle(&fdf->map.ang[Z_AXIS], -ang);
 }
 
 // ? the signature is based on the prototypes found in the MLX library
